ad55a1b4c6ec37ff73aee59c878a379ea7c56dbb
Francisco Lima <francisco1111.fl@protonmail.com>
2025-09-09 14:02:09 -0300

    feat: exercicio media entre tres numeros

---
+++ 02_operadores_e_expressoes/ex008.py
@@ -0,0 +1,7 @@
+primeiro_valor = int(input("Infome um valor inteiro: "))
+segundo_valor = int(input("Infome um segundo valor inteiro: "))
+terceiro_valor = int(input("Infome um terceiro valor inteiro: "))
+
+media = (primeiro_valor + segundo_valor + terceiro_valor) / 3
+
+print(f"A madia entre os tres valores é: {media:.2f}")
