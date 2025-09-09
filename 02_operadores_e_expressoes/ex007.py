2a5257fa18d14e9a9676a3e9c06ed9ddc985d139
Francisco Lima <francisco1111.fl@protonmail.com>
2025-09-09 13:54:56 -0300

    feat: exercicio verificacao de maior valor

---
+++ 02_operadores_e_expressoes/ex007.py
@@ -0,0 +1,7 @@
+primeiro_valor = int(input("Informe um valor inteiro: "))
+segundo_valor = int(input("Informe um segundo valor: "))
+
+if primeiro_valor > segundo_valor:
+    print(f"{primeiro_valor} é o maior valor.")
+elif segundo_valor > primeiro_valor:
+    print(f"{segundo_valor} é o maior valor.")
